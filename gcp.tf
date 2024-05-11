95f6e4e48c70308377442b305b86ce80b2671471
ausmanki <100470886+ausmanki@users.noreply.github.com>
2024-05-11 13:06:10 +0530

    Update gcp.tf

--- gcp.tf
+++ gcp.tf
@@ -11,7 +11,6 @@ resource "random_id" "bucket_suffix" {
 resource "google_storage_bucket" "dynamic_bucket" {
   name          = "my-bucket-${random_id.bucket_suffix.hex}"
   location      = "US"
-  force_destroy = true
   uniform_bucket_level_access = true
   force_destroy = true
 
