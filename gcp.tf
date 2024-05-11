29e6f6d59db67368d603a55b53fa2249288c2f32
ausmanki <100470886+ausmanki@users.noreply.github.com>
2024-05-11 10:22:33 +0530

    Update gcp.tf

--- gcp.tf
+++ gcp.tf
@@ -3,11 +3,12 @@ variable "location" {
   default     = "us-central1c"
   type        = string
 }
+
 resource "google_storage_bucket" "terragoat_website" {
   name          = "terragot-${var.environment}"
   location      = var.location
   force_destroy = true
- }
+}
 
 resource "google_storage_bucket_iam_binding" "allow_public_read" {
   bucket  = google_storage_bucket.terragoat_website.id
