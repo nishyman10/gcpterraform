3d23ec39af01f5cf8d2e1df39036753752bfe458
ausmanki <100470886+ausmanki@users.noreply.github.com>
2024-05-11 11:31:59 +0530

    Update gcp.tf

--- gcp.tf
+++ gcp.tf
@@ -1,28 +1,22 @@
+provider "google" {
+  project = "fleet-garage-421904"  # Replace this with your actual Google Cloud project ID
+  region  = "us-central1"          # Specify your desired region
+  zone    = "us-central1-a"        # Specify your desired zone
+}
 
 resource "random_id" "bucket_suffix" {
-
-byte_length = 4
-
+  byte_length = 4
 }
-
 
 resource "google_storage_bucket" "dynamic_bucket" {
+  name          = "my-bucket-${random_id.bucket_suffix.hex}"
+  location      = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
 
-name = "my-bucket-${random_id.bucket_suffix.hex}"
+  versioning {
+    enabled = true
+  }
 
-location = "US"
-
-force_destroy = true
-
-uniform_bucket_level_access = true
-
-versioning {
-
-enabled = true
-
-}
-
-public_access_prevention = "enforced"
-
-
+  public_access_prevention = "enforced"
 }
